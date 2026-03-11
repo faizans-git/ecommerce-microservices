cae0014d31c1f9e2b9688692062b917e0637a415
faizans-git <frog17male@gmail.com>
2026-03-11 22:19:04 +0500

    feat(rate-limiter): integrate Redis for request rate limiting and add rate limiter middleware

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -5,12 +5,15 @@ import logger from "./utils/logger.js";
 import router from "./routes/auth.js";
 import cors from "cors";
 import helmet from "helmet";
+import { apiRateLimiter } from "./middlewares/rateLimiter.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+
+app.use(apiRateLimiter);
 
 app.use(errorHandler);
 
