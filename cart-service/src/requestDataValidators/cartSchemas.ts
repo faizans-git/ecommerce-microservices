a00e6378c0bbeac20175001c6588e63a0a23dd88
faizans-git <frog17male@gmail.com>
2026-04-05 16:11:18 +0500

    feat: add maximum limit to quantity in cart schemas

--- cart-service/src/requestDataValidators/cartSchemas.ts
+++ cart-service/src/requestDataValidators/cartSchemas.ts
@@ -2,10 +2,10 @@ import Joi from "joi";
 
 export const addToCartSchema = Joi.object({
   variantId: Joi.string().uuid().required(),
-  quantity: Joi.number().integer().min(1).required(),
+  quantity: Joi.number().integer().min(1).required().max(90),
 });
 
 export const updateQuantitySchema = Joi.object({
   variantId: Joi.string().uuid().required(),
-  quantity: Joi.number().integer().min(0).required(),
+  quantity: Joi.number().integer().min(0).required().max(90),
 });
